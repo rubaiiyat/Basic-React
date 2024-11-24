109b61f2a8b48c10da8b18438f3dcd8c10a6ea59
rubaiiyat <abirr1516@gmail.com>
2024-11-24 18:35:09 +0600

    Pass Array

---
+++ 01-first-project/src/Actor.jsx
@@ -0,0 +1,3 @@
+export default function Actor({ name }) {
+  return <p className="text-start">Name: {name}</p>;
+}
--- 01-first-project/src/App.jsx
+++ 01-first-project/src/App.jsx
@@ -3,8 +3,10 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Todo from "./Todo";
+import Actor from "./Actor";
 
 function App() {
+  const actors = ["Ananta", "Jayed khan", "Hero Alam", "Sakib khan"];
   return (
     <>
       <Products name="Mobile" price="200"></Products>
@@ -15,6 +17,11 @@ function App() {
       <Todo task="Learn Javascript" isDone={false}></Todo>
       <Todo task="Learn Python" isDone={false}></Todo>
       <Todo task="Learn Django" isDone={true}></Todo>
+
+      <Actor name={"Bappa"}></Actor>
+      {actors.map((actor) => (
+        <Actor name={actor}></Actor>
+      ))}
     </>
   );
 }
