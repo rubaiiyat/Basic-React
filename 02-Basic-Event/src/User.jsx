72059644de8c39bf2c14d3344d3f6a06bcb42530
rubaiiyat <abirr1516@gmail.com>
2024-11-25 17:34:50 +0600

    show data using useEffect

---
+++ 02-Basic-Event/src/User.jsx
@@ -0,0 +1,12 @@
+export default function User({ user }) {
+  const { name, email, phone } = user;
+  return (
+    <>
+      <div className="border border-green-500 p-10 w-96 text-start ">
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>Phone: {phone}</p>
+      </div>
+    </>
+  );
+}
--- 02-Basic-Event/src/Users.jsx
+++ 02-Basic-Event/src/Users.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import User from "./User";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
@@ -12,6 +13,12 @@ export default function Users() {
   return (
     <>
       <h1>Users: {users.length}</h1>
+
+      <div className="flex flex-wrap gap-5 mt-5 justify-center item-center">
+        {users.map((user) => (
+          <User user={user}></User>
+        ))}
+      </div>
     </>
   );
 }
